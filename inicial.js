b466db7819cb72eccf4a7664406e83157a914211
oLuizEnr <luizenrique80025030@gmail.com>
2024-09-16 16:49:26 -0300

    Tela inical quase finalizada

--- inicial.js
+++ inicial.js
@@ -4,7 +4,7 @@ setInterval(function() {
     document.getElementById('slide' + contador).checked = true; // Marca o slide correspondente ao contador
     contador++; // Incrementa o contador
 
-    if (contador > 5) { // Se o contador ultrapassar 5
+    if (contador > 3) { // Se o contador ultrapassar 3
         contador = 1; // Reseta o contador para 1
     }
-}, 3000); // Executa a função a cada 3 segundos (3000 ms)
+}, 4000); // Executa a função a cada 3 segundos (4000 ms)
