763aa6513d7864e501d9b812d46e75725d7ac862
Lucas-Beni <lucgarcbeni@gmail.com>
2024-09-19 00:45:21 -0300

    página do produto melhorada

---
+++ descricao.js
@@ -0,0 +1,19 @@
+function mudar1(){
+    let principal = document.getElementById("foto");
+    principal.style.backgroundImage = "url('img/Brinco-1.2-removebg-preview.png')";
+}
+
+function mudar2(){
+    let principal = document.getElementById("foto");
+    principal.style.backgroundImage = "url('img/brinco1-1.jfif')";
+}
+
+function mudar3(){
+    let principal = document.getElementById("foto");
+    principal.style.backgroundImage = "url('img/brinco1-2.jfif')";
+}
+
+function mudar4(){
+    let principal = document.getElementById("foto");
+    principal.style.backgroundImage = "url('img/brinco1-3.jfif')";
+}
